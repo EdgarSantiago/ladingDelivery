27669ca20415eca2929e022a0d3dc65c915a7a14
EdgarSantiago <edgargsp777@gmail.com>
2023-08-08 23:25:53 -0300

    xd

--- src/components/index/InfinitySwiper.tsx
+++ src/components/index/InfinitySwiper.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Container, Flex, Heading, Image, Text } from "@chakra-ui/react";
 import ButtonG from "../global/ButtonG";
 import Marquee from "react-fast-marquee";
 
@@ -25,48 +25,22 @@ export default function InfinitySwiper() {
 const SwiperZ = () => {
   const coolTexts = [
     {
-      text: "SELL FOR $5/MO 😎",
-      textColor: "#c4e6ce",
+      src: "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExejVjN3JscDRoZmpsdDNuaHU0b2t0bXFpcDFpeGJsc3hwdTE5N2YyayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xT0xePyGsKplOK5dHG/giphy.gif",
     },
     {
-      text: "SELL FOR $5/MO 🤑",
-      textColor: "#c3eccf",
+      src: "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGkzMGd3anJmNzFkYTl3ZnFnMnVjc3lnMGc5ZDFkeGszY29jbnZrdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7btR4w5GK8CTpNOU/giphy.gif",
     },
     {
-      text: "SELL FOR $5/MO 🚀",
-      textColor: "#bce5c8",
+      src: "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbXZ6azV6dXltZDhpM2wzZnIycXNscTAyY3QybWpiOHp0eHpzdDVlcSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/TsTfcZAHsP3jBbr0JV/giphy.gif",
     },
     {
-      text: "SELL FOR $5/MO 💰",
-      textColor: "#b3e1c1",
+      src: "https://media3.giphy.com/media/R41OuGtgoPhrWOX9dO/giphy.gif?cid=ecf05e474ei5na72wb7fg4vnq3z7tjo28ngrosdcf09h1tpn&ep=v1_gifs_related&rid=giphy.gif&ct=g",
     },
     {
-      text: "SELL FOR $5/MO 🔥",
-      textColor: "#a9e0ba",
+      src: "https://media3.giphy.com/media/XbgtclqMhrlx7PozAs/giphy.gif?cid=ecf05e47u4s8eikt4h1d05mjarw9vkmkqx8jj2xzju0vje8d&ep=v1_gifs_related&rid=giphy.gif&ct=g",
     },
     {
-      text: "SELL FOR $5/MO 👍",
-      textColor: "#9adbaf",
-    },
-    {
-      text: "SELL FOR $5/MO 🌟",
-      textColor: "#9adbaf",
-    },
-    {
-      text: "SELL FOR $5/MO 💎",
-      textColor: "#91deaa",
-    },
-    {
-      text: "SELL FOR $5/MO 🌈",
-      textColor: "#85dca1",
-    },
-    {
-      text: "SELL FOR $5/MO 🎉",
-      textColor: "#7ce39d",
-    },
-    {
-      text: "SELL FOR $5/MO 👏",
-      textColor: "#79E99A",
+      src: "https://media3.giphy.com/media/JVNOiUgDsyS34Jkj5q/giphy.gif?cid=ecf05e47if6pfpm78vtpj5f3ycdj19809raesuhzz0rj5jqx&ep=v1_gifs_related&rid=giphy.gif&ct=g",
     },
   ];
 
@@ -77,12 +51,19 @@ const SwiperZ = () => {
       speed={100}
       autoFill={false}
       gradientWidth={200}
-      style={{ minWidth: "100%" }}
+      style={{ minWidth: "100%", paddingBottom: 20, paddingTop: 20 }}
     >
       {coolTexts.map((v, y) => (
-        <Box bg="red" h="50vh" w="200px">
-          <Text>teste</Text>
-        </Box>
+        <Image
+          border="4px"
+          className="shadow-color-swiper"
+          src={v.src}
+          objectFit={"cover"}
+          h="50vh"
+          w="250px"
+          mx={10}
+          rounded="md"
+        />
       ))}
     </Marquee>
   );
--- src/components/index/Navbar.tsx
+++ src/components/index/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
   return (
     <Flex zIndex={9999} bg="#79E99A" top={0} position={"sticky"} height="100px">
       <Container display={"flex"} justifyContent={"space-between"} minW="8xl">
-        <Heading my="auto">NanoDev</Heading>
+        <Heading my="auto">Teste123</Heading>
         <ButtonG my="auto">Assinar Agora</ButtonG>
       </Container>
     </Flex>
